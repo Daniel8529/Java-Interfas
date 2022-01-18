db7495a56a9afc67a2ed4b2bead8c62af1446396
Daniel8529 <97201981+Daniel8529@users.noreply.github.com>
2022-01-17 17:35:17 -0800

    Add files via upload

---
+++ Registro de usuario/src/registro/de/usuario/RegistroDeUsuario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package registro.de.usuario;
+
+/**
+ *
+ * @author Daniel
+ */
+public class RegistroDeUsuario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Ventanax m= new Ventanax();
+         m.setVisible(true);
+        m.setLocationRelativeTo(null);
+    }
+    
+}
